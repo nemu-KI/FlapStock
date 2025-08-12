c842d3013c394ad564bb08bd672cf5bd2ad95664
nemu-KI <06kousuke0125@gmail.com>
2025-08-12 17:25:19 +0900

    不要ファイルの削除と一部修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,3 @@
 //= link_tree ../builds
 //= link_directory ../stylesheets .css
 //= link application.js
-//= link controllers/application.js
-//= link controllers/delete_confirmation_controller.js
-//= link controllers/index.js
