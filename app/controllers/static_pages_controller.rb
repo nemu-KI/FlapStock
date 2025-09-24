24959d072817e04765242c77d2acebfb135e3267
nemu-KI <06kousuke0125@gmail.com>
2025-09-25 00:53:48 +0900

    プライバシーポリシーの実装

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  # プライバシーポリシーは認証不要でアクセス可能
+  skip_before_action :authenticate_user!, only: [:privacy_policy]
+
+  def privacy_policy
+    # ログイン状態に応じてレイアウトとビューを切り替え
+    if user_signed_in?
+      render layout: 'application'
+    else
+      render 'privacy_policy_public', layout: 'public'
+    end
+  end
+end
