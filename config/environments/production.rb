9b54216f824044344988e6182e26a393650f652c
nemu-KI <06kousuke0125@gmail.com>
2025-10-26 22:21:17 +0900

    UI修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Ensure assets are precompiled
   config.assets.precompile += %w[controllers/application.js controllers/index.js]
@@ -40,6 +40,10 @@ Rails.application.configure do
   # Ensure proper asset serving
   config.assets.digest = true
   config.assets.version = '1.0'
+
+  # Render.com用の設定
+  config.assets.cache_store = :memory_store
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
