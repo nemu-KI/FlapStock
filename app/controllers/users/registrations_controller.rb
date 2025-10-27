ed2453ceebf564bfd8d48d3d4d210e256ca1c333
nemu-KI <06kousuke0125@gmail.com>
2025-10-27 23:13:21 +0900

    軽微な修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,6 +7,19 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :set_ogp_tags, only: [:new]
 
+    def create
+      # 利用規約とプライバシーポリシーの同意チェック
+      unless params[:privacy_policy_agreement].present? && params[:terms_of_service_agreement].present?
+        build_resource(sign_up_params)
+        resource.validate
+        resource.errors.add(:base, '利用規約とプライバシーポリシーへの同意が必要です')
+        render :new, status: :unprocessable_entity
+        return
+      end
+
+      super
+    end
+
     private
 
     def set_ogp_tags
